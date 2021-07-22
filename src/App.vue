dc0e6464dc9152a57f6375d5a37d6a76b21e980f
carmhack <carmhack@gmail.com>
2021-07-22 12:02:49 +0200

    Change font from import to link

--- src/App.vue
+++ src/App.vue
@@ -18,8 +18,6 @@
 </template>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap');
-
 #app {
   width: 100%;
   height: calc(100vh - 100px);
